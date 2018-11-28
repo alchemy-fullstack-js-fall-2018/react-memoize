979391295d0f1f447c4a766383edecb9900ec449
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-11-28 13:06:48 -0800

    all functionality, need to test reactMemo

--- src/index.js
+++ src/index.js
@@ -1,36 +1,39 @@
-// function reactMemoize(fn) {
-//   let previousArgs = [];
-//   let cachedResult;
-//   return function() {
-//     const args = [...arguments];
-//     console.log(args);
-//     if(args === previousArgs) {
-//       return cachedResult;
-//     }
-//     previousArgs = args;
-//     cachedResult = fn(args);
-//     return cachedResult;
-//   };
-// }
+import React from 'react';
 
 export const defaultEquality = (itemA, itemB) => {
-  if(itemA === itemB) {
-    return true;
+  if(!itemA || itemA.length !== itemB.length) {
+    return false;
   }
-  return false;
+  return itemA.every((_, index) => itemA[index] === itemB[index]);
 };
 
-export const reactPropsEquality = (newProps, prevProps) => {
+export const memoize = (fn, equality = defaultEquality) => {
+  let prevArgs = null;
+  let result = null;
+
+  return function() {
+    const args = [...arguments];
+    if(equality(prevArgs, args)) return result;
+    prevArgs = args;
+    return result = fn.apply(null, args);
+  };
+};
+
+
+export const reactPropsEquality = ([newProps], [prevProps]) => {
+  if(prevProps === null || newProps === null) return false;
+
   const newLength = Object.keys(newProps).length;
   const prevLength = Object.keys(prevProps).length;
+
   if(prevLength !== newLength) {
     return false;
   }
+  return Object.keys(prevProps).every(key => newProps[key] === prevProps[key]);
+};
 
-  for(let i = 0; i < prevLength; i++) {
-    if(!defaultEquality(prevProps[i], newProps[i])){
-      return false;
-    }
-  }
-  return true;
+
+
+export const reactMemoize = (Component, equality = reactPropsEquality) => {
+  return memoize(Component, equality);
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,17 +1,40 @@
-import { defaultEquality, reactPropsEquality } from './index';
+import { defaultEquality, reactPropsEquality, memoize, reactMemoize } from './index';
+
+describe('memoize', () => {
+  const equals = defaultEquality;
+  it('returns a function', () => {
+    const func = memoize();
+    expect(func).toBeInstanceOf(Function);
+  });
+
+  it('calls a function', () => {
+    const func = jest.fn();
+    const aFn = memoize(func, equals);
+    aFn();
+    expect(func.mock.calls.length).toBe(1);
+  });
+
+  it('only calls a function when same arguments are passed to it', () => {
+    const func = jest.fn();
+    const aFn = memoize(func, equals);
+    aFn(1);
+    aFn(1);
+    expect(func.mock.calls.length).toBe(1);
+  });
+});
 
 describe('defaultEquality', () => {
   it('returns true if items are equal', () => {
-    const itemA = 2;
-    const itemB = 2;
+    const itemA = [2];
+    const itemB = [2];
     const equal = defaultEquality(itemA, itemB);
 
     expect(equal).toBe(true);
   });
 
   it('returns false if items are not equal', () => {
-    const itemA = 2;
-    const itemB = 4;
+    const itemA = [2];
+    const itemB = [4];
 
     const equal = defaultEquality(itemA, itemB);
 
@@ -21,8 +44,8 @@ describe('defaultEquality', () => {
 
 describe('reactPropsEquality', () => {
   it('returns true if props are equal', () => {
-    const props1 = { x: 1 };
-    const props2 = { x: 1 };
+    const props1 = [{ x: 1 }];
+    const props2 = [{ x: 1 }];
 
     const equal = reactPropsEquality(props1, props2);
 
@@ -30,6 +53,16 @@ describe('reactPropsEquality', () => {
   });
 
   it('returns false if props are not equal', () => {
-    
+    const props1 = [{ x: 1, y: 4 }];
+    const props2 = [{ x: 1 }];
+
+    const equal = reactPropsEquality(props1, props2);
+
+    expect(equal).toBe(false);
   });
 });
+
+describe('reactMemoize', () => {
+
+})
+
