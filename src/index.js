81e24bebd3761b184c1a5eac8087abb007f8f702
mlamb <mlamb128@gmail.com>
2018-11-29 12:14:13 -0800

    All tests passing

--- src/index.js
+++ src/index.js
@@ -9,30 +9,33 @@ export function memoize(fn, equality = defaultEquality) {
       return result;
     }
     return result;
-  }
-};
+  };
+}
 
 
 export function defaultEquality(prevArgs, newArgs) {
-  if(!prevArgs || !newArgs) return false;
-  return prevArgs === newArgs;
-};
+  if(!prevArgs || !newArgs || prevArgs.length !== newArgs.length) return false;
+
+  return prevArgs.every((arg, i) => {
+    return arg === newArgs[i];
+  });
+}
 
 export function reactPropsEquality(prevProps, newProps) {
   if(!prevProps || !newProps) return false;
 
   const prevPropsKeys = Object.keys(prevProps);
   const newPropsKeys = Object.keys(newProps);
-  if (prevPropsKeys.length !== newPropsKeys.length) return false;
+  if(prevPropsKeys.length !== newPropsKeys.length) return false;
 
-  for (let i = 0; i < prevPropsKeys.length; i++) {
+  for(let i = 0; i < prevPropsKeys.length; i++) {
     const key = prevPropsKeys[i];
-    if (!defaultEquality(prevProps[key], newProps[key])) {
+    if(!defaultEquality([prevProps[key]], [newProps[key]])) {
       return false;
     }
   }
   return true;
-};
+}
 
 export function reactMemoize(Component) {
   return memoize(Component, reactPropsEquality);
--- src/index.test.js
+++ src/index.test.js
@@ -5,25 +5,43 @@ describe('memoization', () => {
   describe('memoize function', () => {
     const equality = defaultEquality;
     it('returns a function', () => {
-      const add = function (a, b) {
-        return a + b ;
-      }
-      const result = memoize(add(1, 2), equality);
+      const fn = jest.fn();
+      const result = memoize(fn, equality);
       expect(typeof result).toBe('function');
-    })
-  })
+    });
+
+    it('calls the function passed as params once', () => {
+      const fn = jest.fn();
+      const result = memoize(fn, equality);
+      result();
+      expect(fn.mock.calls.length).toBe(1);
+    });
+
+    it('calls the function passed as params once when arguments are the same', () => {
+      const fn = jest.fn();
+      const result = memoize(fn, equality);
+      result(1);
+      result(1);
+      expect(fn.mock.calls.length).toBe(1);
+    });
+  });
 
   describe('default equality function', () => {
     it('returns false if args are not equal', () => {
-      const result = defaultEquality(1, 2);
+      const result = defaultEquality([1], [2]);
       expect(result).toBeFalsy();
-    })
+    });
+
+    it('returns false if different lengths', () => {
+      const result = defaultEquality([1, 2], [1, 2, 3]);
+      expect(result).toBeFalsy();
+    });
 
     it('returns true if args are not equal', () => {
-      const result = defaultEquality(1, 1);
+      const result = defaultEquality([1, 2], [1, 2]);
       expect(result).toBeTruthy();
-    })
-  })
+    });
+  });
 
   describe('reactPropsEquality function', () => {
     it('returns false if args are not equal', () => {
@@ -31,24 +49,37 @@ describe('memoization', () => {
       const arg2 = { a: 1, b: 3 };
       const result = reactPropsEquality(arg1, arg2);
       expect(result).toBeFalsy();
-    })
+    });
 
     it('returns true if args are equal', () => {
       const arg1 = { a: 1, b: 2 };
       const arg2 = { a: 1, b: 2 };
       const result = reactPropsEquality(arg1, arg2);
       expect(result).toBeTruthy();
-    })
-  })
+    });
+  });
 
   describe('reactMemoize', () => {
-    it('returns a Component', () => {
-      const Component = function TestFunc() {
-        return(<h1>I am a test</h1>)
-      }
 
+    it('returns a Component', () => {
+    const Component = jest.fn();
       const result = reactMemoize(Component);
       expect(typeof result).toEqual('function');
-    })
-  })
-})
+    });
+
+    it('calls the function passed as params once', () => {
+      const Component = jest.fn();
+      const result = reactMemoize(Component);
+      result();
+      expect(Component.mock.calls.length).toBe(1);
+    });
+
+    it('calls the function passed as params once', () => {
+      const Component = jest.fn();
+      const result = reactMemoize(Component);
+      result();
+      result();
+      expect(Component.mock.calls.length).toBe(1);
+    });
+  });
+});
